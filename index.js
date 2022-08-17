4b0071f77e67700a98e32c305aac6a98b1281a9a
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-17 21:59:40 +0300

    scope chain in js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code your solution here
+// function saturdayFun 
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adjective) {
+  if (adjective === "*") {
+    return () => `You are ${adjective}a hard worker${adjective}!`;                         // alternatie      // const asterick = () => `You are ${adjective}a hard worker${adjective}!`; return asterick;
+  } else if (adjective === "||") {
+    return () => `You are ${adjective}a dedicated programmer${adjective}!`;                 //alternative     // const pipe = () => `You are ${adjective}a dedicated programmer${adjective}!`; return pipe;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// Alternative for easier readability of code
+
+
+// function wrapAdjective(adjective="special"){
+//     if(adjective==="*"){
+//         return function(){
+//             return `You are ${adjective}a hard worker${adjective}!`;
+//         }
+//     }else if (adjective ==="||" ){
+//         return function(){
+//             return `You are ${adjective}a dedicated programmer${adjective}!`; 
+//         }
+//     }
+// }
